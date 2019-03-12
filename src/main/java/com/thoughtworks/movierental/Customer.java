d5f71ba314d8534cacadf3a9a25861ce523eea8d
Samyuktha <samyuj15@gmail.com>
2019-03-12 22:33:45 +0530

    Refactoring totalAmount and frequent rental points

--- src/main/java/com/thoughtworks/movierental/Customer.java
+++ src/main/java/com/thoughtworks/movierental/Customer.java
@@ -7,6 +7,10 @@ public class Customer {
   private String name;
   private List<Rental> rentals = new ArrayList<>();
 
+  private int frequentRenterPoints = 0;
+  private double totalAmount = 0;
+
+
   public Customer(String name) {
     this.name = name;
   }
@@ -20,28 +24,29 @@ public class Customer {
   }
 
   public String statement() {
-    double totalAmount = 0;
-    int frequentRenterPoints = 0;
     String result = "Rental Record for " + getName() + "\n";
     for (Rental each : rentals) {
-      double thisAmount = each.amount();
-      frequentRenterPoints = getFrequentRenterPoints(frequentRenterPoints, each);
-
       //show figures for this rental
       result += "\t" + each.getMovie().getTitle() + "\t" +
-          String.valueOf(thisAmount) + "\n";
-      totalAmount += thisAmount;
+          String.valueOf(each.amount()) + "\n";
     }
 
     //add footer lines result
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+    result += "Amount owed is " + String.valueOf(getTotalAmount()) + "\n";
     result += "You earned " + String.valueOf(frequentRenterPoints)
         + " frequent renter points";
     return result;
   }
 
-  private int getFrequentRenterPoints(int frequentRenterPoints, Rental each) {
-    // add frequent renter points
+  private double getTotalAmount() {
+    for (Rental each : rentals) {
+      frequentRenterPoints = getFrequentRenterPoints(each);
+      totalAmount += each.amount();
+    }
+    return totalAmount;
+  }
+
+  private int getFrequentRenterPoints(Rental each) {
     frequentRenterPoints++;
     // add bonus for a two day new release rental
     if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
